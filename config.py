271ab72b52327767f1fda3cf85d805dc7e37ec5d
Evgeniy Badosov <eugenotipe@gmail.com>
2026-09-21 11:07:41 +0700

    database-url changed

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     app_name: str = "Event Provider API"
-    database_url: str = "postgresql+asyncpg://eventprovider:secret@localhost:5432/eventprovider"
+    database_url: str = "postgresql+asyncpg://eventprovider:secret@eventprovider-db:5432/eventprovider"
 
     model_config = SettingsConfigDict(env_file=".env", env_prefix="", extra="ignore")
 
--- crud.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import select
-from sqlalchemy import DateTime
-from sqlalchemy.ext.asyncio import AsyncSession
-from models import Event, Place
-from schemas import EventsList
-
-async def get_events(db:AsyncSession, changed_at: DateTime) -> EventsList:
